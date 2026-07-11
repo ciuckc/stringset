da93df86e371c68d529e5202019ccc2a0ae455c4
scristia <scristian@student.codam.nl>
2026-07-11 17:40:17 +0200

    feat: create add/contains/remove skeleton functions for hashset
    also added size and load factor functionality

--- include/stringhashset/stringhashset.hpp
+++ include/stringhashset/stringhashset.hpp
@@ -6,13 +6,26 @@ namespace containers {
 class StringHashSet {
 public:
     StringHashSet(size_t bucket_count);
+    StringHashSet() = default;
+    StringHashSet(const StringHashSet&) = default;
+    StringHashSet& operator=(const StringHashSet&) = default;
+    StringHashSet(StringHashSet&&) = default;
+    StringHashSet& operator=(StringHashSet&&) = default;
+    ~StringHashSet() = default;
+
+    bool add(const std::string& data);
+    bool remove(const std::string& data);
+    bool contains(const std::string& data);
+    size_t size() const noexcept;
+    float loadFactor() const noexcept;
 
 private:
     struct Node {
-        std::string key;
-        bool occupied;
+        std::string key{};
+        bool occupied{false};
     };
 
-    std::vector<Node> map;
+    std::vector<Node> map{};
+    size_t count{};
 };
 } // namespace containers
--- src/stringhashset/stringhashset.cpp
+++ src/stringhashset/stringhashset.cpp
@@ -4,5 +4,28 @@
 
 namespace containers {
 StringHashSet::StringHashSet(size_t bucket_count)
-    : map(bucket_count) {}
+    : map(bucket_count),
+    count(0) {}
+
+bool StringHashSet::add(const std::string& data) {
+    (void)data;
+    return true;
 }
+
+bool StringHashSet::remove(const std::string& data) {
+    (void)data;
+    return true;
+}
+bool StringHashSet::contains(const std::string& data) {
+    (void)data;
+    return true;
+}
+
+size_t StringHashSet::size() const noexcept {
+    return this->count;
+}
+
+float StringHashSet::loadFactor() const noexcept {
+    return this->map.size() != 0 ? static_cast<float>(this->count) / this->map.size() : 0.0f;
+}
+} // namespace containers
